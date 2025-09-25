d066ac698ab083ca479f5019babbb6a113e9e91c
muzianqashmal <muzian24ti@mahasiswa.pcr.ac.id>
2025-09-25 10:13:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/matakuliah/show/{id?}', [MatakuliahController::class, 'show']);
 Route::get('/mahasiswa/{zian}',[MahasiswaController::class, 'show']);
 
 Route::get('/pcr', function () {
-    return 'aku raja minyak';
+    return 'aku raja minyakkkkk';
 });
 
 Route::get('/mahasiswa', function () {
