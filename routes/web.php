ef6fe61348580ffaab1beecaf180fa54dfe3d824
Neysa <muzian24ti@mahasiswa.pcr.ac.id>
2025-10-16 10:33:00 +0700

    pertemuan6bpf

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
